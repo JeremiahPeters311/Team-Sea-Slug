e1c8b1482ce8b693bc005ddbfa56af04a51efd25
Raccoon0223 <colestran@gmail.com>
2025-02-09 12:26:07 -0600

    WindowEnemy Animations

--- Assets/ColeBranchStuff/Scripts/WIndowEnemy.cs
+++ Assets/ColeBranchStuff/Scripts/WIndowEnemy.cs
@@ -4,27 +4,15 @@ using UnityEngine;
 
 public class PlantEnemy : MonoBehaviour
 {
-    [SerializeField] private float spawnInterval;
-    private float spawnTime;
-
     [SerializeField] private GameObject objectToDrop;
-
-    // Update is called once per frame
-    void Update()
-    {
-        spawnTime += Time.deltaTime;
-        if (spawnTime > spawnInterval)
-        {
-            Spawn();
-            spawnTime = 0f;
-        }
-    }
 
     /// <summary>
     /// Function that spawns the potted plant to fall when it's time to do so
     /// </summary>
     private void Spawn()
     {
+        Debug.Log("Plant Spawn");
+
         var offset = new Vector3(0f, -0.2f, 0f);
 
         Instantiate(objectToDrop, gameObject.transform.position + offset, Quaternion.identity);
