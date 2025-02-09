af9269061aee9902819cbffa9d7b5ec64f2239c7
Raccoon0223 <colestran@gmail.com>
2025-02-09 14:15:33 -0600

    WindowEnemy Offscreen Pause

--- Assets/ColeBranchStuff/Scripts/WIndowEnemy.cs
+++ Assets/ColeBranchStuff/Scripts/WIndowEnemy.cs
@@ -6,15 +6,33 @@ public class PlantEnemy : MonoBehaviour
 {
     [SerializeField] private GameObject objectToDrop;
 
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.enabled = false;
+    }
+
     /// <summary>
     /// Function that spawns the potted plant to fall when it's time to do so
     /// </summary>
     private void Spawn()
     {
-        Debug.Log("Plant Spawn");
-
         var offset = new Vector3(0f, -0.2f, 0f);
 
         Instantiate(objectToDrop, gameObject.transform.position + offset, Quaternion.identity);
+    }
+
+    private void OnBecameVisible()
+    {
+        anim.enabled = true;
+        enabled = true;
+    }
+
+    private void OnBecameInvisible()
+    {
+        anim.enabled = false;
+        enabled = false;
     }
 }
