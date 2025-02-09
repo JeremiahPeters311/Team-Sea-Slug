a9a9d45380db6a838b1d0f29cfa39c107e416752
JeremiahPeters <jcpeters@mail.bradley.edu>
2025-02-08 21:46:48 -0600

    title screen

--- Assets/Scripts/TitleScreenBehaviour.cs
+++ Assets/Scripts/TitleScreenBehaviour.cs
@@ -10,6 +10,12 @@ public class TitleScreenScrolling : MonoBehaviour
 
     [SerializeField] private Canvas _sceneCanvas;
 
+    [SerializeField] private Animator Anim;
+
+    [SerializeField] private Animator FiredAnim;
+
+    [SerializeField] private float AnimationWaitTime;
+
     private bool _hasScrollingStarted = false;
 
     private void Update()
@@ -20,8 +26,19 @@ public class TitleScreenScrolling : MonoBehaviour
             {
                 Debug.LogWarning("scroll speed is set to zero, now it won't scroll, please fix that, thanks");
             }
-            StartCoroutine(ScrollingScreen(_movingDestination.position, _screenScrollSpeed));
+            
+            StartCoroutine(IntroTiming());
+            //StartCoroutine(ScrollingScreen(_movingDestination.position, _screenScrollSpeed));
         }
+    }
+
+    private IEnumerator IntroTiming()
+    {
+        FiredAnim.SetBool("bro is fired", true);
+        yield return new WaitForSeconds(1.5f);
+        Anim.SetBool("Game Started", true);
+        yield return new WaitForSeconds(1);
+        StartCoroutine(ScrollingScreen(_movingDestination.position, _screenScrollSpeed));
     }
 
     private IEnumerator ScrollingScreen(Vector3 destination, float scrollSpeed)
@@ -34,7 +51,7 @@ public class TitleScreenScrolling : MonoBehaviour
                 transform.position = Vector3.MoveTowards(transform.position, new Vector3(destination.x, destination.y, transform.position.z),
                     scrollSpeed * (_sceneCanvas.renderingDisplaySize.x / 100) * Time.deltaTime);
 
-                //for whatever reason (probably rounding bs) this coroutine doesn't actually stop itself properly when done
+                //for whatever reason (probably float bs) this coroutine doesn't actually stop itself properly when done
                 //this fixes that
                 if (Mathf.Approximately(transform.position.y, destination.y))
                 {
