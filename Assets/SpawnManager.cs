0e6d2239d10a106fe35e84697f3109f3aac993c0
DalstenYan <dyan@mail.bradley.edu>
2025-02-08 22:19:36 -0600

    Spawn Manager & Spawn Nodes Made
    Made the Spawn Manager and Spawn Nodes, and ensured functions were working properly
    -DY

---
+++ Assets/SpawnManager.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Enemies to spawn per second")]
+    [Range(0f,5f)]
+    private float _spawnRate = 1;
+    [SerializeField]
+    private int _maxEnemies = 2;
+
+    [SerializeField]
+    bool _spawnOfficeWorkers;
+    [SerializeField]
+    GameObject _officeWorkerPrefab;
+
+    [SerializeField]
+    List<GameObject> existingEnemiesInScene;
+    [SerializeField]
+    List<SpawnNodeScript> existingNodesStack;
+
+    private void Start()
+    {
+        existingNodesStack = new();
+        StartCoroutine(SpawnEnemyRoutine());
+    }
+
+    IEnumerator SpawnEnemyRoutine() 
+    {
+        yield return new WaitForSeconds(1f * (1 / _spawnRate));
+        SendEnemyToSpawnNode();
+        StartCoroutine(SpawnEnemyRoutine());
+        
+    }
+
+    void SendEnemyToSpawnNode() 
+    {
+        //iterate backwards like a Stack
+        for (int i = existingNodesStack.Count - 1; i >= 0; i--) 
+        {
+            if (existingEnemiesInScene.Count < _maxEnemies)
+            {
+                var instantiatedEnemy = Instantiate(_officeWorkerPrefab, existingNodesStack[i].transform);
+                existingEnemiesInScene.Add(instantiatedEnemy);
+                existingNodesStack[i].PushEnemyOnNode(instantiatedEnemy);
+            }
+            else { break; }
+            
+        }
+    }
+
+    public void AddSpawnNode(SpawnNodeScript node, float newSpawnRate, int newMaxEnemies) 
+    {
+        var possibleMatch = existingNodesStack.Find(n => n.GetID() == node.GetID());
+        if (possibleMatch != null) 
+        {
+            existingNodesStack.Remove(possibleMatch);
+        }
+        existingNodesStack.Add(node);
+        _spawnRate = newSpawnRate;
+        _maxEnemies = newMaxEnemies;
+    }
+}
---
+++ Assets/SpawnNodeScript.cs
@@ -0,0 +1,35 @@
+using System;
+using UnityEngine;
+
+public class SpawnNodeScript : MonoBehaviour
+{
+    [SerializeField]
+    private string NodeID;
+    SpawnManager spawnManager;
+
+    [SerializeField]
+    private float sectionSpawnRate;
+    [SerializeField]
+    private int sectionMaxEnemies;
+
+    private void Start()
+    {
+        NodeID = Guid.NewGuid().ToString();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player")) 
+        {
+            spawnManager = GameObject.Find("SpawnManager").GetComponent<SpawnManager>();
+            spawnManager.AddSpawnNode(this, sectionSpawnRate, sectionMaxEnemies);
+        }
+    }
+
+    public string GetID() { return NodeID; }
+
+    public void PushEnemyOnNode(GameObject enemy) 
+    {
+        enemy.transform.position += Vector3.up;
+    }
+}
